366a481b5b2d98655825bbe7c81f9db026bc9134
Gabriel Estrella <rolyestrella21@gmail.com>
2023-01-19 21:55:41 -0500

    first javascript code

---
+++ index.js
@@ -0,0 +1,13 @@
+function on(){
+    const turnOnButton = document.getElementById('bulb').src='assets/bulbon.png';
+    return turnOnButton;
+}
+
+function off(){
+    const turnOnButtoff = document.getElementById('bulb').src='assets/bulboff.png';
+    return turnOnButtoff;
+    }
+class user {
+    name =  "gabriel";
+    lastname =  "Estrella"
+}console.log(user.name)
